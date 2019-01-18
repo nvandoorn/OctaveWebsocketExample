923feb28fc42086ba2c7ba5fd2c8591d2ac1cf1f
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-18 14:35:23 -0800

    Use fetch instead of node https
    I also applied prettier with the default settings... sorry for making
    this diff a little messy.

--- hello_octave.js
+++ hello_octave.js
@@ -1,60 +1,54 @@
-const config = require('./config');
-const https = require('https');
-const WebSocket = require('ws');
+const process = require("process");
+const fetch = require("node-fetch");
+const WebSocket = require("ws");
+
+const config = require("./config");
+
+const HOSTNAME = config.octave.url || "octave-ws.sierrawireless.io";
 
 function octave_requestWebsocketSessionId() {
-    const options = {
-        hostname: 'octave-ws.sierrawireless.io',
-        port: 443,
-        path: '/session',
-        method: 'POST',
-        headers: {
-            'X-Auth-User':  config.octave.username,
-            'X-Auth-Company': config.octave.company,
-            'X-Auth-Token': config.octave.masterToken
-        }
-    };
-    return new Promise(function(resolve, reject) {
-        const req = https.request(options, (res) => {
-            if (res.statusCode != 201) {
-                resolve(new Error(`Unexpected status code (${res.statusCode})`));
-            }
-
-            const chunks = [];
-            res.on('data', (d) => {
-                chunks.push(d);
-            });
-            res.on('end', () => {
-                const b = Buffer.concat(chunks);
-                const s = b.toString();
-                const j = JSON.parse(s);
-                resolve(j.body.id);
-            });
-        });
-
-        req.on('error', (e) => {
-            reject(e);
-        });
-        req.end();
-    });
+  // I didn't inspect the fetch API that closely
+  // but it should be mostly compatible with https from node
+  const options = {
+    port: 443,
+    path: "/session",
+    method: "POST",
+    headers: {
+      "X-Auth-User": config.octave.username,
+      "X-Auth-Company": config.octave.company,
+      "X-Auth-Token": config.octave.masterToken
+    }
+  };
+  return fetch(HOSTNAME, options)
+    .then(r => Promise.resolve(r.json()))
+    .then(
+      p =>
+        new Promise((resolve, reject) => {
+          p.statusCode !== 201
+            ? reject(new Error(`Unexpected status code (${res.statusCode})`))
+            : resolve(p.body.id);
+        })
+    );
 }
 
-octave_requestWebsocketSessionId().then(
-    id => {
-        console.log(id);
-        const ws = new WebSocket(`wss://octave-ws.sierrawireless.io/session/${id}/ws`);
-        ws.on('open', () => {
-            console.log("Websocket established");
-            const subReq = {
-                "msgId": "my-path-request",
-                "object": "event",
-                "type": "subscribe",
-                "path": `/${config.octave.company}/devices/${config.octave.device}/light`
-            };
-            ws.send(JSON.stringify(subReq));
-        });
-        ws.on('message', (data) => {
-            console.log(data);
-        });
-    },
-    error => console.log(error));
+octave_requestWebsocketSessionId()
+  .then(id => {
+    console.log(id);
+    const ws = new WebSocket(
+      `wss://octave-ws.sierrawireless.io/session/${id}/ws`
+    );
+    ws.on("open", () => {
+      console.log("Websocket established");
+      const subReq = {
+        msgId: "my-path-request",
+        object: "event",
+        type: "subscribe",
+        path: `/${config.octave.company}/devices/${config.octave.device}/light`
+      };
+      ws.send(JSON.stringify(subReq));
+    });
+    ws.on("message", data => {
+      console.log(data);
+    });
+  })
+  .catch(e => console.log(e));
